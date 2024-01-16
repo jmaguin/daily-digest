3095caada83ff266459fa6af1b695883b4199229
Jack Maguin <jmaguin@scu.edu>
2024-01-15 22:49:06 -0800

    updated progressBar class

--- ProgressBar.py
+++ ProgressBar.py
@@ -5,13 +5,14 @@ class ProgressBar:
 
     # Constructor
     # Initialize progress bar
-    def __init__(self, page_range):
+    def __init__(self, page_range, tag):
+        self.tag = tag
         self.cursor = 0
         self.page_range = page_range
         self.progress_bar = ["."] * (page_range + 2)
         self.progress_bar[0] = "["
         self.progress_bar[len(self.progress_bar)-1] = "]"
-        print("\r%d of %d pages complete %s" % (0, self.page_range, self.prog_bar_string()), end="")
+        print("\n\rTag: %s\t| %d of %d pages complete %s" % (self.tag, 0, self.page_range, self.prog_bar_string()), end="")
 
     # Converts progress_bar list to string
     def prog_bar_string(self):
@@ -23,5 +24,5 @@ class ProgressBar:
         if self.cursor < self.page_range: 
             self.cursor+=1
             self.progress_bar[self.cursor] = "="
-        print("\r%d of %d pages complete %s" % (self.cursor, self.page_range, self.prog_bar_string()), end="")
+        print("\rTag: %s\t| %d of %d pages complete %s" % (self.tag, self.cursor, self.page_range, self.prog_bar_string()), end="")
     
