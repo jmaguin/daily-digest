fa9d57686623553fd81be35c737dedf49ad1fc8f
Jack Maguin <jmaguin@scu.edu>
2024-01-19 15:06:34 -0800

    created NPR scraper

---
+++ scraper/Npr.py
@@ -0,0 +1,121 @@
+#file: Npr.py
+import requests
+import time
+import warnings
+from Article import Article
+from bs4 import BeautifulSoup
+from ProgressBar import ProgressBar
+from WebScraper import WebScraper
+
+
+# Class to gather data from NPR
+# Base URL: https://www.npr.org/
+class Npr(WebScraper):
+
+    # Initialize the object
+    def __init__(self):
+        print("/--------------- NPR scraper started. ---------------/")
+        baseURL = "https://www.npr.org/sections/"
+        page_range = 1 # how many pages to get articles from
+        
+        # List of all tags for NPR
+        self.tags = ["national",
+                     "world",
+                     "politics",
+                     "business",
+                     "health",
+                     "science",
+                     "climate",
+                     "race",
+                     "culture",
+                     "movies",
+                     "food",
+                     "gaming",
+                     "television"
+                    ] 
+
+        # List of all articles from NPR
+        self.articles = []
+
+        self.source_name = "NPR"
+        self.art_count = 0   # num of articles found for this tag
+        
+        start = time.time()
+        # Build list of all relevant articles
+        for tag in self.tags:
+            prog_bar = ProgressBar(page_range, tag)
+
+            # Get articles
+            tag_home_page = requests.get(baseURL + tag) # get page
+            tag_home_soup = BeautifulSoup(tag_home_page.content, "html.parser")         # soupify
+
+            # Grab links to all articles from current tag on current page
+            # header_tags: Type ResultSet
+            links_set = set()
+            header_tags = tag_home_soup.find_all("h2")
+            for header in header_tags:
+                a_tag = header.find("a")
+                if a_tag:
+                    links_set.add(a_tag)
+            
+            # Retrieve articles from links_set
+            # Add articles to self.articles
+            for link in links_set:
+                article = self.create_article(tag, link["href"])    # create Article object
+                if article is not None:
+                    self.articles.append(article)   # append Article to list
+                    self.art_count+=1
+            
+            prog_bar.update_progress()
+        
+        end = time.time()
+        print("\n\nSOURCE: NPR | ELAPSED TIME: %dsec | ARTICLES LOGGED: %d\n" % (end-start, self.art_count))
+        print("/--------------- NPR scraper completed. ---------------/")
+        
+
+    # Retrieve all information about article from its URL
+    # Returns Article object -> None if fails
+    def create_article(self, tag, url):
+        req = requests.get(url)   # generate URL
+        soup = BeautifulSoup(req.content, "html.parser")  # create soup from URL
+
+        # get title
+        try:
+            title = soup.find("h1").get_text()
+        except Exception as e:
+            warnings.warn("\nWARNING: Could not locate article title.\n\tArticle: %s\n\tError: %s" % (url, e))
+            return None
+
+        # get date
+        try: 
+            date_block = soup.find("div", class_="dateblock")
+            time_tag = date_block.find("time")
+            date = time_tag["datetime"]
+        except Exception as e:
+            warnings.warn("\nWARNING: Could not locate article date.\n\tArticle: %s\n\tError: %s" % (url, e))
+            return None
+
+        # get content
+        try:
+            storytext = soup.find("div", class_="storytext")
+            p_tags = storytext.find_all("p", recursive=False)    # filter to remove image caption, etc.
+            content = ""
+
+            # assemble all paragraphs into single string
+            for p in p_tags:
+                content += p.get_text()
+        except Exception as e:
+            warnings.warn("\nWARNING: Could not locate article content.\n\tArticle: %s\n\tError: %s" % (url, e))
+            return None
+
+        return Article(tag, title, self.source_name, date, url, content)
+
+    # Print out all found articles
+    def print_articles(self):
+        for tag in self.tags:
+            count = 1
+            print("\n------------ " + "TAG: " + tag + " ------------\n")
+            for article in self.articles:
+                if (article.tag == tag):
+                    print("(" + str(count) + ") " + article.title)
+                    count+=1
