8afc9845fac3425170b55c32d69053c65b5b9282
Jack Maguin <jmaguin@scu.edu>
2024-01-19 16:24:08 -0800

    created master list of tags

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,6 @@
 #file: main.py
 import sys
+from Ap import Ap
 from bs4 import BeautifulSoup
 from Npr import Npr
 from PbsNewshour import PbsNewshour
@@ -8,26 +9,56 @@ from Database import Database
 
 print("Program running!\n")
 
+# master list of all tags
+master_tags = ["politics",
+                "world",
+                "sports",
+                "entertainment",
+                "business",
+                "science",
+                "health",
+                "climate",
+                "technology",
+                "religion",
+                "nation",
+                "arts",
+                "economy",
+                "education",
+                "race",
+                "culture",
+                "movies",
+                "food",
+                "gaming",
+                "television"
+                ]
+
 # Instantiate database
 db = Database()
 
 # Instantiate scrapers
 # pbs_newshour = PbsNewshour()
-npr = Npr()
-# ap = Ap()
+# npr = Npr()
+ap = Ap()
 
 # Add PBS Newshour articles to database
 # for article in pbs_newshour.articles:
     # db.insert_article(article)
 
 # Add NPR articles to database
-for article in npr.articles:
+# for article in npr.articles:
     # Keep tag names consistent. (PBS uses "nation")
-    if(article.tag == "national"):
-        article.tag = "nation"
-    db.insert_article(article)
+    # if(article.tag == "national"):
+        # article.tag = "nation"
+    # db.insert_article(article)
 
 # Add AP articles to database
-# tags = ap.get_tags()
-# add_articles_to_db("Associated Press", ap.get_articles())
+for article in ap.articles:
+    # Keep tag names consistent
+    if(article.tag == "world-news"):
+        article.tag = "world"
+    elif(article.tag == "climate-and-environment"):
+        article.tag = "climate"
+    elif(article.tag == "us-news"):
+        article.tag = "nation"
+    db.insert_article(article)
 
