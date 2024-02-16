226878d2279b615c9f2bb6762ef05442e3769712
Jack Maguin <jmaguin@scu.edu>
2024-02-16 14:37:16 -0800

    cleaned up language_model

--- language_model/summarizer.py
+++ language_model/summarizer.py
@@ -2,21 +2,14 @@
 
 import time
 import sys
-from gpt4all import GPT4All
-from model_enum import Model
 sys.path.insert(1, "../database")
 from Database import Database
 
 print("Running ...")
 start_time = time.time()
 
-model = GPT4All(str(Model.MISTRAL.value))  # create model
+# Open AI Chat GPT code here
 
-with model.chat_session():
-    model.generate("What is the longest month?", max_tokens=40)
-    output = model.current_chat_session[2]['content']
-
-# Print output and execution time
-print(output)
+# Print execution time
 end_time = time.time()
 print("---------- Execution time: %ssec ----------" % round(end_time-start_time, 2))
--- models/model_folder.md
+++
@@ -1,3 +0,0 @@
-# Model Folder
-
-This folder contains all of the ".gguf" models for GPT4All. The models are used in /language_model/summarizer.py.
