96d49b3ee267bea0657839a1bd1e71dd19038f83
Jack Maguin <jmaguin@scu.edu>
2024-02-23 17:46:23 -0800

    added article click functionality

--- website/web_main.py
+++ website/web_main.py
@@ -1,62 +1,35 @@
-import sys
 from datetime import datetime as dt
 from pyscript import document
 from pyweb import pydom
 
-articles = []
+# Color palette -> from index.css
+gray = "#444444"
+dark_gray = "#313131"
+darkest_gray = "#292929"
+text_color = "#c0c0c0"
+accent_color = "#58a858"
+dark_accent_color = "#438143"
+darkest_accent_color = "#346634"
 
-def q(selector, root=document):
-    return root.querySelector(selector)
+all_articles = []   # all articles for current topic
+selected_articles = []  # tracks all articles the user has selected
+max_selection = 3   # max num of articles that can be selected
 
-# Instantiate database
-# db = Database()
+# called when article clicked
+def article_clicked(event):
+    article = event.currentTarget
+    
+    # if article not already clicked
+    if article not in selected_articles:
+        # if num of selected articles lower than max allowed
+        if len(selected_articles) < max_selection:
+            selected_articles.append(article)
+            article.style.backgroundColor = darkest_gray
+    # unclick article
+    else:
+        selected_articles.remove(article)
+        article.style.backgroundColor = gray
 
-# article and dropdown templates
-# used to add new articles
-article_template = pydom.Element(q("#article-template").content.querySelector("label"))
-dropdown_template = pydom.Element(q("#dropdown-template").content.querySelector("option"))
-
-article_list = pydom["#list-tasks-container"][0]
-new_article_content = pydom["#new-task-content"][0]
-
-
-def add_task(e):
-    # ignore empty task
-    if not new_article_content.value:
-        return None
-
-    # create task
-    task_id = f"task-{len(articles)}"
-    article = {
-        "id": task_id,
-        "content": new_article_content.value,
-        "done": False,
-        "created_at": dt.now(),
-    }
-
-    articles.append(article)
-
-    # add the task element to the page as new node in the list by cloning from a
-    # template
-    article_html = article_template.clone()
-    article_html.id = task_id
-
-    article_html_check = article_html.find("input")[0]
-    article_html_content = article_html.find("p")[0]
-    article_html_content._js.textContent = article["content"]
-    article_list.append(article_html)
-
-    def check_task(evt=None):
-        article["done"] = not article["done"]
-        article_html_content._js.classList.toggle("line-through", article["done"])
-
-    new_article_content.value = ""
-    article_html_check._js.onclick = check_task
-
-
-def add_task_event(e):
-    if e.key == "Enter":
-        add_task(e)
-
-
-new_article_content.onkeypress = add_task_event
+# called when generate button clicked
+def generate(event):
+    print("generate!")
