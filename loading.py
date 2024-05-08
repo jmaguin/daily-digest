780351204215472aefded60b5e3038bceaaa4e63
Jack Maguin <jmaguin@scu.edu>
2024-05-07 22:26:08 -0700

    OpenAI HTTP request; summary page working

--- loading.py
+++ loading.py
@@ -2,15 +2,14 @@ from js import localStorage
 from js import setInterval
 from js import clearInterval
 from js import window
+from js import console, XMLHttpRequest
 from pyscript import document
 from pyweb import pydom
 from Article import *
 from pyodide.ffi import create_proxy
-from dotenv import load_dotenv, find_dotenv
-from openai import OpenAI
 import config
 import local_storage
-import os
+import json
 
 # list of articles selected on index.html (Article objects)
 selected_articles = local_storage.retrieve_articles()
@@ -46,10 +45,10 @@ interval_id = setInterval(timer_proxy, 1000)  # call update_timer() every 1sec
 
 
 # LLM Code ------------------------------------------->
-summary = "Error"
 
-load_dotenv(find_dotenv())  # load API key from .env
-client = OpenAI(api_key=os.getenv("API_KEY"))   # Open connection to OpenAI using API_KEY
+# Get API Key from file
+f = open("key.txt", "r")
+OPENAI_API_KEY = f.read()
 
 # LLM variables
 model = "gpt-4"
@@ -58,32 +57,42 @@ selected_articles_content = ""
 
 # Iterate thru selected_articles and combine their content
 for article in selected_articles:
-    selected_articles_content = selected_articles_content + "\n" + article.content
-
-print(selected_articles_content)
+    article.content = article.content[:250].strip().replace("\n", " ")
+    selected_articles_content = selected_articles_content + article.content
 
 messages = [
     {"role": "system", "content": llm_instructions},
     {"role": "user", "content": selected_articles_content}
 ]
 temperature = 0.7
-max_tokens = 1000
+max_tokens = 2000
 top_p = 1
 
-# Generate summary
-completion = client.chat.completions.create(
-    model = model,
-    messages = messages,
-    temperature = temperature,
-    max_tokens = max_tokens,
-    top_p = top_p
-)
 
-summary = completion.choices[0].message.content
-print(summary)
+# HTTP Request ---------------------------------->
 
+xhr = XMLHttpRequest.new()
+xhr.open("POST", "https://api.openai.com/v1/chat/completions", False)
+xhr.setRequestHeader("Content-Type", "application/json")
+xhr.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+
+data = json.dumps({
+    "model": model,
+    "messages": messages,
+    "temperature": temperature,
+    "max_tokens": max_tokens,
+    "top_p": top_p,
+})
+
+xhr.send(data)
+
+json_response = json.loads(xhr.response)
+print(json_response)
+summary = json_response["choices"][0]["message"]["content"]
+
+# -------------------------------------<
 local_storage.save_articles_summary(summary)    # save summary to localStorage
+clearInterval(interval_id)  # stop counter
+window.location.href = "summary.html" # redirect to summary page
 # ------------------------------------------------------------<
 
-window.location.href = "summary.html" # redirect to summary page
-clearInterval(interval_id)  # stop counter
